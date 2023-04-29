8f676a5091bd97e6aa732362e9993ef53175941f
kalshaer <kalshaer@MacBook-Air.local>
2023-04-29 23:12:20 +0400

    1

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,7 +6,7 @@
 /*   By: kalshaer <kalshaer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 23:48:34 by kalshaer          #+#    #+#             */
-/*   Updated: 2023/04/27 22:02:35 by kalshaer         ###   ########.fr       */
+/*   Updated: 2023/04/29 22:46:41 by kalshaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,16 @@ int	parceing_check(int argc, char **argv)
 	if (check_is_digit(argv))
 	{
 		printf("Invalid arguments\n");
+		return (1);
+	}
+	if (ft_atoi(argv[1]) > 200 || ft_atoi(argv[1]) < 0)
+	{
+		printf("Can't test more than 200 philo or less that 0\n");
+		return (1);
+	}
+	if (ft_atoi(argv[2]) < 60 || ft_atoi(argv[3]) < 60 || ft_atoi(argv[4]) < 60)
+	{
+		printf("the time can't be less that 60\n");
 		return (1);
 	}
 	return (0);
